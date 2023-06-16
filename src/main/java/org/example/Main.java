2dd928c7b541eb959dc1966484d94ead38fef65a
Fly <67563726+FlyUltra@users.noreply.github.com>
2023-06-16 23:41:47 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+       String str = "Zpráva!";
+
+       int num = 1;
+       double num2 = 1.5;
+
+    }
+
+
+
+}
